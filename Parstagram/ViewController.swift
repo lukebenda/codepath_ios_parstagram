1d27390012c4f8e4a0fd95728ccfd62b39a4325e
Lucas Benda <54371703+lukebenda@users.noreply.github.com>
2022-10-08 16:00:02 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+// Parstagram
+// ViewController.swift
+// Created by Lucas Benda on  10/8/22 AT 3:59 PM.
+// 
+// Swift version  5.0
+// Running on macOS 12.5
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
