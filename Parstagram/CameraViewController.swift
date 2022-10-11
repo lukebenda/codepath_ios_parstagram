6531d3b61b3c26413c09251a2388066fe9a607d9
Lucas Benda <54371703+lukebenda@users.noreply.github.com>
2022-10-10 23:24:03 -0500

    Post a photo and save the post

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -8,34 +8,54 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var commentField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image?.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            }
+            else {
+                print("Error!")
+            }
+        }
     }
     
-     @IBAction func onCameraButton(_ sender: Any) {
-         let picker = UIImagePickerController()
-         picker.delegate = self
-         picker.allowsEditing = true
-         
-         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-             picker.sourceType = .camera
-         } else {
-             picker.sourceType = .photoLibrary
-         }
-         
-         present(picker, animated: true, completion: nil)
-     }
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
@@ -50,12 +70,12 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     /*
      // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
